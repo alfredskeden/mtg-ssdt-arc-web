c507ef1f0552b3e871de8b53e47ac44d283a3846
alfredskeden <alfred.skedeback91@gmail.com>
2023-09-27 11:25:19 +0200

    remove linting in builds

--- next.config.js
+++ next.config.js
@@ -21,6 +21,11 @@ module.exports = withPWA({
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // !! WARN !!
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 });
